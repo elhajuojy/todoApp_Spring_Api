de82c5e3b8586719ad987d1ceeea3c6e7bf82be0
elhajuojy <souirimehdi311@gmail.com>
2023-05-09 10:24:54 +0100

    First Test

--- src/test/java/com/elhjuojy/todoapp_spring/FirstTest.java
+++ src/test/java/com/elhjuojy/todoapp_spring/FirstTest.java
@@ -1,15 +1,30 @@
 package com.elhjuojy.todoapp_spring;
 
+import com.elhjuojy.todoapp_spring.model.User;
+import com.elhjuojy.todoapp_spring.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
 @SpringBootTest
 public class FirstTest{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Test
     void test(){
-        
+
         Assertions.assertEquals(2,2);
 
+    }
+
+    @Test
+    void test1(){
+        User user = userRepository.save(new User(null,"user22@test.com","user1","123",new ArrayList<>()));
+        Assertions.assertSame(user,user);
     }
 }
